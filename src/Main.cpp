05b9ce21163dd5bda34d7c01599866a625f062f2
Paulchch <83615379+Paulchch@users.noreply.github.com>
2022-03-15 22:23:58 -0600

    Update Main.cpp
    Codigo para pruebas en main

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,14 +3,18 @@
 //
 #include<iostream>
 #include"Derived1.h"
+#include"Derived2.h"
 int main(){
     Base b1;
     Derived1 d1;
+    Derived2 d2;
     b1.do_something();
     d1.do_something();
+    d2.do_something();
     std::cout<< std::endl;
     std::cout<<b1.toString();
     std::cout<<d1.toString();
+    std::cout<<d2.toString();
     //std::cout<< "Hola mundo!" <<std::endl;
     return 0;
 }
